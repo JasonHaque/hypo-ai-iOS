5286aa69ccec166a155a1c3a69c1994fe3e40712
Jason Haque <zahinjason220434@gmail.com>
2023-04-04 15:19:33 +0600

    Initial Commit

---
+++ Hypno-Ai/Hypno_AiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hypno_AiApp.swift
+//  Hypno-Ai
+//
+//  Created by Sanviraj Zahin Haque on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Hypno_AiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
